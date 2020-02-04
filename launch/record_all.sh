e9d36729c33ddb5860cb4bc3a2819c11152666d8
mohradza <mioh0887@colorado.edu>
2020-02-04 12:57:44 -0700

    changes for cart test

--- launch/record_all.sh
+++ launch/record_all.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-rosbag record -o ./bag/  \
-/HD2/assembled_cloud \
-/HD2/constraint_list \
-/HD2/hokuyo_points \
+rosbag record -o ./bag/test1 \
+/HD2/Altitude \
+/HD2/Pressure \
+/HD2/Temperature \
 /HD2/imu_raw \
-/HD2/landmark_poses_list \
-/HD2/map \
 /HD2/os1_cloud_node_h/points \
 /HD2/os1_cloud_node_v/points \
-/HD2/scan \
-/HD2/scan_matched_points2 \
-/HD2/submap_list \
 /HD2/trajectory_node_list \
+/rosout \
+/rosout_agg \
 /tf \
 /tf_static
+
